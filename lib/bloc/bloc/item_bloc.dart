e2e892efb61b25470b00fae2981a242fe2afc60a
YeabseraMesfin <ymesfin2001@gmail.com>
2023-03-31 03:06:26 -0700

    modified item bloc

--- lib/bloc/bloc/item_bloc.dart
+++ lib/bloc/bloc/item_bloc.dart
@@ -1,12 +1,16 @@
+import 'package:asbeza/data%20base/service.dart';
 import 'package:bloc/bloc.dart';
 import 'package:asbeza/bloc/bloc/item_event.dart';
 import 'package:asbeza/bloc/bloc/item_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../data/model/api.dart';
 
 class ItemBloc extends Bloc<ItemEvent, ItemState> {
 final _apiServiceProvider = ApiServiceProvider();
   List history = [];
+  List historyLoad=[];
+  final _service = Service();
   ItemBloc() : super(TestInitialState()) {
     on<AsbezaFetchEvent>((event, emit) async {
       emit(TestLoadingState());
@@ -18,6 +22,16 @@ final _apiServiceProvider = ApiServiceProvider();
       }
     });
 
-    on<HistoryEvent>((event, emit) => {history.add(event.data)});
+   
+  
+   on<HistoryEvent>((event, emit) {
+          if (!historyLoad.contains(event.data))
+            {
+              // _service.wipeDate(),
+              historyLoad.add(event.data);
+              event.data.itemAdded = 1;
+              _service.saveItem(event.data);
+            }
+        });
   }
 }
